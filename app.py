7e8faab477352cc7044e63a63530fea2a5c65b0a
Paolo Morato <pmorato187@gmail.com>
2023-02-24 18:26:07 +0800

    svm

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn.metrics import accuracy_score
 import os
 
@@ -28,10 +28,10 @@ text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', SVC())])
 parameters = {'tfidf__ngram_range': [(1, 1), (1, 2), (2, 2)],
               'tfidf__use_idf': (True, False),
               'clf__C': [0.1, 1, 10, 100],
-              'clf__kernel': ['linear', 'rbf', 'poly']}
+              'clf__kernel': ['linear', 'rbf', 'poly', 'sigmoid']}
 
-# Search for the best hyperparameters using Grid Search
-clf = GridSearchCV(text_clf, parameters, cv=5)
+# Search for the best hyperparameters using Randomized Search
+clf = RandomizedSearchCV(text_clf, parameters, n_iter=10, cv=5, random_state=42)
 clf.fit(X_train, y_train)
 
 # Use the trained model to predict the sentiment of the test set
